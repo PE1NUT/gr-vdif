ea8f7fe279f582ddf57ff0e1f068fb831dd95784
Paul Boven <p.boven@xs4all.nl>
2020-09-22 21:20:31 +0200

    Properly setting the number of channels in the vdif header

--- lib/VDIF_packetize_impl.cc
+++ lib/VDIF_packetize_impl.cc
@@ -71,7 +71,9 @@ namespace gr {
         /* Fill in the actual VDIF header */
         vh.ref_epoch = hye;
         vh.frame_length = (frame_length + 32)/8;
-        vh.bits_sample = 1;
+        vh.bits_sample = 1; // bits/sample - 1 
+        auto Log2 = [](int s){int l=0; while( 1<<l != s && l < 16) l++; return l;};
+        vh.log_chans = Log2(streams);
         vh.station_id = 256 * station_code[0] + station_code[1];
         vh.seconds = (time_t)start_time - epoch;
     }
